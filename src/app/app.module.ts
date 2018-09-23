e7d6dc4b455f0a07707152bcfad58ca7644b63e0
Caleb Froese <calebfroese@Calebs-iMac.local>
2018-09-23 13:20:21 +0930

    Add user feature state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
+import { metaReducers, reducers } from './reducers';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot([AppEffects])],
+  imports: [
+    BrowserModule,
+    UserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([AppEffects]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

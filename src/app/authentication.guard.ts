feb6307c9e37f911fb5742c740b078a7f1333ae3
Caleb Froese <calebfroese@Calebs-iMac.local>
2018-09-23 20:11:01 +0930

    Navigate out when failing to log in

--- src/app/authentication.guard.ts
+++ src/app/authentication.guard.ts
@@ -3,22 +3,30 @@ import {
   ActivatedRouteSnapshot,
   CanActivate,
   RouterStateSnapshot,
+  Router,
 } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { getLoggedIn } from './user/reducers/user.reducer';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationGuard implements CanActivate {
-  constructor(public store: Store<any>) {}
+  constructor(public store: Store<any>, public router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return this.store.select(getLoggedIn);
+    return this.store.select(getLoggedIn).pipe(
+      tap(loggedIn => {
+        if (!loggedIn) {
+          this.router.navigate(['/login']);
+        }
+      })
+    );
   }
 }

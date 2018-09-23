e8545739558966ff6c5f1f15e20f9ec6ade3915e
Caleb Froese <calebfroese@Calebs-iMac.local>
2018-09-23 15:35:43 +0930

    Convert to a sass project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   form: FormGroup;

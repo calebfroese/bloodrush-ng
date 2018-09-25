6fe929578d9671350ef8a5b3d2b1d72798b2f7de
Caleb Froese <caleb.froese@gmail.com>
2018-09-25 22:04:19 +0930

    Add missing test coverage for logout button

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,11 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { StoreModule } from '@ngrx/store';
+import { By } from '@angular/platform-browser';
+import { Store, StoreModule } from '@ngrx/store';
 
+import { Logout } from '../user/actions/user.actions';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let store: Store<any>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,9 +21,17 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('dispatches event to log out', () => {
+    const logoutButton = fixture.debugElement.query(By.css('button'));
+    logoutButton.nativeElement.click();
+    expect(store.dispatch).toHaveBeenCalledWith(new Logout());
   });
 });
